2639f91f7085836651ca19cc6a8f2e03ef227ab4
dann109 <de229@cornell.edu>
2017-06-14 20:34:14 -0400

    Create BBB.cpp

--- BBB.cpp
+++ BBB.cpp
@@ -1,6 +1,7 @@
 /* Basic script to test that BlackLib.h can compile
 To compile this, you must have a copy of the BlackLib-master directory in your current directory.
 Compile with "g++ BBB.cpp -std=c++11" OR "g++ BBB.cpp -std=gnu++11"
+Then run the program with "./a.out"
 */
 
 // v3_0 is the latest version of BlackLib. v2 and v1 are in BlackLib-master incase they are needed.
